7025042e1134048daf42cba88647dae09c19b2d1
lzwjava <lzwjava@gmail.com>
2023-11-26 00:44:06 +0800

    add

--- remove_comment.py
+++ remove_comment.py
@@ -6,6 +6,8 @@ with open('code.txt', 'r') as file:
 # Find and remove all multiline comments
 cleaned_code = re.sub(r'(?:^|\n)\s*""".*?"""', '', code, flags=re.DOTALL)
 
+cleaned_code = re.sub(r'#.*', '', cleaned_code)
+
 # Write cleaned code to new file
 with open('cleaned_code.txt', 'w') as file:
     file.write(cleaned_code)
